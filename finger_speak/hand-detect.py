f321b0e7d0f8f79197782f7dbe112e6e0144b78a
moayadabukhadra <moayadabukhadra54@gmail.com>
2022-04-25 23:49:27 +0300

    fingers_status_base

--- finger_speak/hand-detect.py
+++ finger_speak/hand-detect.py
@@ -8,10 +8,37 @@ class SignDetection:
         self.hands = self.mp_hands.Hands()
         self.mp_draw = mp.solutions.drawing_utils
         self.cap = cv2.VideoCapture(3)
+        
 
     def hand_detection(self, cap):
+        # Initialize the finger status with None
+            index_tip_status_fh = None
+            index_tip_status_fv = None
+            index_tip_status_v = None
+            index_tip_status_h = None
+            middle_tip_status_fh = None
+            middle_tip_status_fv = None
+            middle_tip_status_v = None
+            middle_tip_status_h = None
+            little_tip_status_fh = None
+            little_tip_status_fv = None
+            little_tip_status_v = None
+            little_tip_status_h = None
+            ring_tip_status_fh = None
+            ring_tip_status_fv = None
+            ring_tip_status_v = None
+            ring_tip_status_h = None
+            thumb_tip_status_fh = None
+            thumb_tip_status_fv = None
+            thumb_tip_status_v = None
+            thumb_tip_status_h = None
         while True:
-
+            finger_tips = [8, 12, 16, 20]
+            thumb_tip = 4
+            index_tip = 8
+            middle_tip = 12
+            ring_tip = 16
+            little_tip = 20
             ret, img = cap.read()
             if img is None:
                 break
@@ -24,6 +51,64 @@ class SignDetection:
                     lm_list = []
                     for id, lm in enumerate(hand_landmark.landmark):
                         lm_list.append(lm)
+                        finger_fold_status = []
+                        for tip in finger_tips:
+                            x, y = int(lm_list[tip].x * w), int(lm_list[tip].y * h)
+
+                            # index_tip
+                            if lm_list[index_tip - 2].y < lm_list[index_tip - 1].y < lm_list[index_tip].y:
+                                index_tip_status_v = "down"
+                            if lm_list[index_tip].y < lm_list[index_tip - 1].y < lm_list[index_tip - 2].y:
+                                index_tip_status_v = "up"
+                            if lm_list[index_tip].x < lm_list[index_tip - 1].x < lm_list[index_tip - 2].x:
+                                index_tip_status_h = "left"
+                            if lm_list[index_tip - 2].x < lm_list[index_tip - 1].x < lm_list[index_tip].x:
+                                index_tip_status_h = "right"
+                            if lm_list[index_tip].y < lm_list[index_tip - 2].y:
+                                index_tip_status_fv = "fold up"
+                            if lm_list[index_tip - 2].y < lm_list[index_tip].y:
+                                index_tip_status_fv = "fold down"
+                            if lm_list[index_tip].x < lm_list[index_tip - 2].x:
+                                index_tip_status_fh = "fold right"
+                            if lm_list[index_tip - 2].x < lm_list[index_tip].x:
+                                index_tip_status_fh = "fold left"
+
+                            # middle_tip
+                            if lm_list[middle_tip - 2].y < lm_list[middle_tip - 1].y < lm_list[middle_tip].y:
+                                middle_tip_status_v = "down"
+                            if lm_list[middle_tip].y < lm_list[middle_tip - 1].y < lm_list[middle_tip - 2].y:
+                                middle_tip_status_v = "up"
+                            if lm_list[middle_tip].x < lm_list[middle_tip - 1].x < lm_list[middle_tip - 2].x:
+                                middle_tip_status_h = "left"
+                            if lm_list[middle_tip - 2].x < lm_list[middle_tip - 1].x < lm_list[middle_tip].x:
+                                middle_tip_status_h = "right"
+                            if lm_list[middle_tip].y < lm_list[middle_tip - 2].y:
+                                middle_tip_status_fv = "fold up"
+
+                            if lm_list[middle_tip - 2].y < lm_list[middle_tip].y:
+                                middle_tip_status_fv = "fold down"
+                            if lm_list[middle_tip].x < lm_list[middle_tip - 2].x:
+                                middle_tip_status_fh = "fold right"
+                            if lm_list[middle_tip - 2].x < lm_list[middle_tip].x:
+                                middle_tip_status_fh = "fold left"
+
+                            # ring_tip
+                            if lm_list[ring_tip - 2].y < lm_list[ring_tip - 1].y < lm_list[ring_tip].y:
+                                ring_tip_status_v = "down"
+                            if lm_list[ring_tip].y < lm_list[ring_tip - 1].y < lm_list[ring_tip - 2].y:
+                                ring_tip_status_v = "up"
+                            if lm_list[ring_tip].x < lm_list[ring_tip - 1].x < lm_list[ring_tip - 2].x:
+                                ring_tip_status_h = "left"
+                            if lm_list[ring_tip - 2].x < lm_list[ring_tip - 1].x < lm_list[ring_tip].x:
+                                ring_tip_status_h = "right"
+                            if lm_list[ring_tip].y < lm_list[ring_tip - 2].y:
+                                ring_tip_status_fv = "fold up"
+                            if lm_list[ring_tip - 2].y < lm_list[ring_tip].y:
+                                ring_tip_status_fv = "fold down"
+                            if lm_list[ring_tip].x < lm_list[ring_tip - 2].x:
+                                ring_tip_status_fh = "fold right"
+                            if lm_list[ring_tip - 2].x < lm_list[ring_tip].x:
+                                ring_tip_status_fh = "fold left"
 
                     self.mp_draw.draw_landmarks(
                         img,
