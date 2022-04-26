4b0fc719e873763a8e72465d886ad4868649a50d
RaghadAbdulhadi <raghadabdulhadi1999@gmail.com>
2022-04-26 21:02:15 +0300

    Remove unneeded lines of the code

--- finger_speak/volume_control.py
+++ finger_speak/volume_control.py
@@ -18,7 +18,6 @@ class HandSignDetector:
         self.mp_hands = mp.solutions.hands
         self.hands = self.mp_hands.Hands()
         self.mp_draw = mp.solutions.drawing_utils
-        self.tip_ids = [4, 8, 12, 16, 28]
 
     def find_hands(self, img, draw=True):
         """
@@ -63,10 +62,10 @@ def gestures_volume():
         Arguments: None
         Returns: Increases or Decreases the sound
     """
-    wCam, hCam = 640, 480
+    w_cam, h_cam = 640, 480
     cap = cv2.VideoCapture(0)
-    cap.set(3, wCam)
-    cap.set(4, hCam)
+    cap.set(3, w_cam)
+    cap.set(4, h_cam)
     detector = HandSignDetector()
 
     # pycow
@@ -81,22 +80,19 @@ def gestures_volume():
     vol_bar = 400
     vol_per = 0
     while True:
-        success, img = cap.read()
-        img = detector.find_hands(img)
-        lm_list = detector.find_position(img, draw=False)
-        if len(lm_list) != 0:
-            # print(x1, y1)
-            x1, y1 = lm_list[4][1], lm_list[4][2]
-            x2, y2 = lm_list[8][1], lm_list[8][2]
+        success, image = cap.read()
+        img = detector.find_hands(image)
+        landmarks_list = detector.find_position(img, draw=False)
+        if len(landmarks_list) != 0:
+            x1, y1 = landmarks_list[4][1], landmarks_list[4][2]
+            x2, y2 = landmarks_list[8][1], landmarks_list[8][2]
             cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
-            cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
-            cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
-            cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
-            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+            cv2.circle(img, (x1, y1), 15, (255,182,193), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 15, (255,182,193), cv2.FILLED)
+            cv2.circle(img, (cx, cy), 15, (255,255,0), cv2.FILLED)
+            cv2.line(img, (x1, y1), (x2, y2), (176,224,230), 3)
 
             length = math.hypot(x2 - x1, y2 - y1)
-            # print(length)
-
             # hand range 50 - 300
             # volume range -65 - 0
             vol = np.interp(length, [50, 300], [min_vol, max_vol])
@@ -106,19 +102,19 @@ def gestures_volume():
             volume.SetMasterVolumeLevel(vol, None)
 
             if length < 50:
-                cv2.circle(img, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
+                cv2.circle(img, (cx, cy), 15, (128,0,128), cv2.FILLED)
 
-        cv2.rectangle(img, (50, 150), (85, 400), (0, 255, 0), 3)
-        cv2.rectangle(img, (50, int(vol_bar)), (85, 400), (255, 0, 255), cv2.FILLED)
+        cv2.rectangle(img, (50, 150), (85, 400), (255,182,193), 4)
+        cv2.rectangle(img, (50, int(vol_bar)), (85, 400), (128,0,128), cv2.FILLED)
 
         cv2.putText(
             img,
-            f"Volume%: {int(vol_per)}%",
+            f"Speakers: {int(vol_per)}%",
             (40, 50),
             cv2.FONT_HERSHEY_COMPLEX,
             1,
-            (255, 0, 255),
-            3,
+            (128,0,128),
+            2,
         )
         cv2.imshow("Volume Control Detection", img)
         k = cv2.waitKey(1) & 0xFF
